c406fe011196e62c2fc04c928a3316ef4756ea93
L0L-I-P0P <wolffox392@gmail.com>
2023-10-20 15:46:23 +0300

    Track2.0

--- src/Game.js
+++ src/Game.js
@@ -14,11 +14,13 @@ const runInteractiveConsole = require('./keyboard');
 class Game {
   constructor(trackLength) {
     this.trackLength = trackLength;
-    this.hero = new Hero(); // Герою можно аргументом передать бумеранг.
+    this.hero = new Hero(0); // Герою можно аргументом передать бумеранг.
     this.boomerang = new Boomerang();
     this.enemy = new Enemy(trackLength - 1);
     this.view = new View(this);
     this.track = [];
+    this.score = 0;
+    this.timer = 0;
     this.regenerateTrack();
   }
 
@@ -27,13 +29,25 @@ class Game {
     // в единую структуру данных
     this.track = Array(this.trackLength).fill(' ');
     this.track[this.hero.position] = this.hero.skin;
-    this.track[this.boomerang.position] = this.boomerang.skin;
     this.track[this.enemy.position] = this.enemy.skin;
+    if (this.hero.boomerang.position >= 0) {
+      this.track[this.hero.boomerang.position] = this.hero.boomerang.skin;
+    }
+  }
+  timerA() {
+    this.timer += 0.1;
   }
 
   check() {
     if (this.hero.position === this.enemy.position) {
       this.hero.die();
+    }
+    if (
+      this.hero.boomerang.position === this.enemy.position ||
+      this.hero.boomerang.position > this.enemy.position
+    ) {
+      this.enemy.die();
+      this.score += 1;
     }
   }
 
@@ -43,17 +57,39 @@ class Game {
       // Let's play!
       this.check();
       this.regenerateTrack();
+      this.updateEnemy();
+      this.timerA();
       this.view.render(this.track);
       this.enemy.moveLeft();
-    }, 100);
+      this.updateBoomerang();
+      console.log(`Score:${this.score}`);
+      console.log(`Timer:${this.timer.toFixed(1)}`);
+    }, 250);
+  }
+  updateBoomerang() {
+    const boomerang = this.hero.boomerang;
+    if (boomerang.isReturning) {
+      boomerang.moveLeft();
+      if (boomerang.position === this.hero.position) {
+        boomerang.isReturning = false;
+        this.hero.isBoomerangThown = false;
+      } else if (this.hero.isBoomerangThown) {
+        if (boomerang.position < this.enemy.position) {
+          boomerang.moveRight();
+        } else {
+          boomerang.isReturning = true;
+        }
+      }
+    }
   }
   updateEnemy() {
     if (this.enemy.position >= 0 && this.enemy.position < this.trackLength) {
       this.enemy.moveLeft();
     } else {
-      this.enemy = new Enemy(thi.trackLength - 1);
+      this.enemy = new Enemy(this.trackLength - 1);
     }
   }
+  updateBoomerang() {}
 }
 
 module.exports = Game;
