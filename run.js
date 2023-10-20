63c42edb7445151f37178585feeac5e777a7b09f
Lida <lidamalygina@mail.ru>
2023-10-20 15:41:20 +0300

    welcome class

--- run.js
+++ run.js
@@ -2,9 +2,18 @@
 // Запускает игру.
 const Game = require('./src/Game');
 const runInteractiveConsole = require('./src/keyboard');
+const Welcome = require('./welcome');
 
 // Инициализация игры с настройками.
 const game = new Game(30);
-
+const a = new Welcome();
+async function start() {
+  await a.Welcome();
+  await a.getName();
+  await a.getPasword();
+}
 // Запуск игры.
-game.play();
+// console.log('Бумеранг');
+start().then(() => {
+  game.play();
+});
